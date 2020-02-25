a7c5bdf1d79e518c7edd2e356ca75d5f52fa4208
Khari Pierce <56198077+khariP@users.noreply.github.com>
2020-02-25 10:16:44 -0500

    adding more assignments

---
+++ basic_html_and_JavaScript_1.html/script src=Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+<reciept>
+	function My_First_Function() {
+	var string = "Kiss Me, I'm Irish!";
+	var result = "string.fontcolor("green");
+	document.getElemenByid("Irish").innerHTML=
+	result;
+	}
