3fa056e1c4e64875558a22d52065deeffd85c037
Khari Pierce <56198077+khariP@users.noreply.github.com>
2020-04-30 14:57:18 -0400

    New Assignments

--- Project/bootstrap4_project/Basic JavaScript Projects/Challenge12.js
+++ Project/bootstrap4_project/Basic JavaScript Projects/Challenge12.js
@@ -1,6 +1,6 @@
 
     var text = "";
-    var i;
+    var i;  // this is a declaration that assignes i the value of 0
     for (i = 0; i < 10; i++) {
       if (i === 3) { break; }
       text += "The number is " + i + "<br>";
